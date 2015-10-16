e21ec6cfb64bdd20b35296b1e5e57b2801fb9c36
ahtn <ahtn@users.noreply.github.com>
2015-10-16 10:51:32 +1000

    renaming

--- keyboard/split_keyboard/config.h
+++ keyboard/split_keyboard/config.h
@@ -23,9 +23,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x4444
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    testco
-#define PRODUCT         TODO
-#define DESCRIPTION     t.m.k. keyboard firmware for TODO
+#define MANUFACTURER    tmk-split
+#define PRODUCT         tmk-split-keyboard
+#define DESCRIPTION     tmk split keyboard firmware
 
 /* key matrix size */
 #define MATRIX_ROWS 8
